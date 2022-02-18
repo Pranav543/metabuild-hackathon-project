6405d44b5051c40bc60018aa52107635440a3d29
Pranav543 <pranavnaik543@gmail.com>
2022-02-19 01:47:30 +0530

    unfinished rough contract

---
+++ contract/src/location.rs
@@ -0,0 +1,42 @@
+use crate::*;
+
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Default, Clone, Copy)]
+pub struct Location {
+    pub cur_index: Option<Measurement>,
+    // amount of money invested here
+    pub total_invested: Balance,
+    pub current_invested: Balance,
+    // number of individual investments made (people)
+    pub total_investments: u64,
+    pub current_investments: u64,
+}
+
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Default, Clone, Copy)]
+pub struct Measurement {
+    pub value: f64,
+    // unix time (UTC) in seconds
+    pub time: u64,
+}
+
+impl Measurement {
+    pub fn new(value: f64, time: u64) -> Measurement {
+        Measurement { value, time }
+    }
+}
+
+impl Location {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn add_investment(&mut self, amount: Balance) {
+        self.total_invested += amount;
+        self.current_invested += amount;
+        self.total_investments += 1;
+        self.current_investments += 1;
+    }
+
+    pub fn finish_investment(&mut self, amount: Balance, count: u64) {
+        self.current_investments -= count;
+    }
+}
